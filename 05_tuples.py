4a6fe71f7bf25021e2c9351c3b933762e64fcf89
LukaHaziel <lukahazielg@gmail.com>
2025-06-04 10:50:15 -0300

    Update 05_tuples.py
    Puse Muck, Lu, 17,  24, 48

--- 05_tuples.py
+++ 05_tuples.py
@@ -7,8 +7,8 @@
 my_tuple = tuple()
 my_other_tuple = ()
 
-my_tuple = (35, 1.77, "Brais", "Moure", "Brais")
-my_other_tuple = (35, 60, 30)
+my_tuple = (17, 1.69, "Lu", "Muck, "Lu")
+my_other_tuple = (17, 24, 48)
 
 print(my_tuple)
 print(type(my_tuple))
@@ -20,9 +20,9 @@ print(my_tuple[-1])
 # print(my_tuple[4]) IndexError
 # print(my_tuple[-6]) IndexError
 
-print(my_tuple.count("Brais"))
-print(my_tuple.index("Moure"))
-print(my_tuple.index("Brais"))
+print(my_tuple.count("Lu"))
+print(my_tuple.index("Muck"))
+print(my_tuple.index("Lu"))
 
 # my_tuple[1] = 1.80 'tuple' object does not support item assignment
 
